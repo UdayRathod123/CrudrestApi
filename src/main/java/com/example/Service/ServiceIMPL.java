67beda27b197e6364e57513f3be22c491e5b1875
unknown <udaysrathod95@gmail.com>
2022-08-07 12:11:34 +0530

    SirviceIMPL class created

--- src/main/java/com/example/Service/ServiceIMPL.java
+++ src/main/java/com/example/Service/ServiceIMPL.java
@@ -2,17 +2,22 @@ package com.example.Service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.Repository.EmployeeRepository;
 import com.example.model.Employee;
 
 @Service
 public class ServiceIMPL implements ServiceI{
 
+	@Autowired
+	private EmployeeRepository empRepository;
 	@Override
+	
 	public Employee savaData(Employee employee) {
-		// TODO Auto-generated method stub
-		return null;
+		Employee save = empRepository.save(employee);
+		return save;
 	}
 
 	@Override
