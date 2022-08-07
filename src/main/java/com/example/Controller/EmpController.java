3d426c348ec2159582e3bd240d335af1871dfc4d
unknown <udaysrathod95@gmail.com>
2022-08-07 12:31:32 +0530

    controller layar saveEmp method completed

--- src/main/java/com/example/Controller/EmpController.java
+++ src/main/java/com/example/Controller/EmpController.java
@@ -1,5 +1,25 @@
 package com.example.Controller;
 
-public class EmpController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Service.ServiceI;
+import com.example.model.Employee;
+
+@RestController
+public class EmpController {
+@Autowired
+	private ServiceI servicei;
+
+@PostMapping (value = "/addEmployee",consumes = "applition/json")
+public ResponseEntity<Employee> addData(@RequestBody Employee employee) {
+	Employee savaData = servicei.savaData(employee);
+	
+	return new ResponseEntity<>(savaData,HttpStatus.CREATED);
+	
+}
 }
