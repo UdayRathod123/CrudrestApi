18db60991ca7cb7db922076db4ca84b29d98122c
unknown <udaysrathod95@gmail.com>
2022-08-07 21:24:58 +0530

    controller multi method

--- src/main/java/com/example/Controller/EmpController.java
+++ src/main/java/com/example/Controller/EmpController.java
@@ -1,5 +1,7 @@
 package com.example.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Service.ServiceI;
 import com.example.model.Employee;
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
 @RestController
 public class EmpController {
@@ -22,4 +25,17 @@ public ResponseEntity<Employee> addData(@RequestBody Employee employee) {
 	return new ResponseEntity<>(savaData,HttpStatus.CREATED);
 	
 }
+@PostMapping (value= "/addmulti", consumes = "application/json",produces = "application/json")
+public ResponseEntity<List<Employee>>  multiemp(@RequestBody List<Employee> emp){
+	List<Employee> list = servicei.savamultiple(emp);
+	
+	
+	return new ResponseEntity<>(list,HttpStatus.CREATED);
+	
+	
+	
+}
+
+
+
 }
