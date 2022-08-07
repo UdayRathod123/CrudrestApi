99c122af4073a7181a1d96e19bbf3b592125b770
unknown <udaysrathod95@gmail.com>
2022-08-07 21:18:03 +0530

    Student model class

---
+++ src/main/java/com/example/model/Student.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+ 
+	@Id
+	private int sid ;
+	
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getSname() {
+		return sname;
+	}
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	public String getSadd() {
+		return sadd;
+	}
+
+	public void setSadd(String sadd) {
+		this.sadd = sadd;
+	}
+
+	private String sname;
+	
+	private String sadd;
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", sadd=" + sadd + "]";
+	}
+	
+	
+	
+}
